b5531c40084724ffc4fa5794c66573fdcb5d9b6c
Mahdi Islam <84149578+mahdiislam79@users.noreply.github.com>
2025-09-01 01:56:23 +0600

    Refactor uncertainty estimation file paths and logic
    Updated paths and removed filtering for cases with no iliac.

--- uncertainty_estimation.py
+++ uncertainty_estimation.py
@@ -4,15 +4,15 @@ import os
 import shutil
 from tqdm import tqdm
 
-cases_with_no_iliac = [15, 20, 26, 30, 50, 60, 63, 74, 75, 78, 86, 101, 107, 111, 112, 123, 130, 139, 143, 146, 151]
+# cases_with_no_iliac = [15, 20, 26, 30, 50, 60, 63, 74, 75, 78, 86, 101, 107, 111, 112, 123, 130, 139, 143, 146, 151]
 
 # add the cases with no iliac to the list using the number of the case [15 -> case_015.npz]
-cases_with_no_iliac = [f'case_{str(i).zfill(3)}' for i in cases_with_no_iliac]
+# cases_with_no_iliac = [f'case_{str(i).zfill(3)}' for i in cases_with_no_iliac]
 
 # ----- SETTINGS -----
-source_folder = '/gpfs/data/fs71894/mahdi_i/nnunet_accessroute/inference/inference_stage1/labelsTr'
-filtered_folder = '/gpfs/data/fs71894/mahdi_i/nnunet_accessroute/inference/inference_stage1/proba'
-output_path = '/gpfs/data/fs71894/mahdi_i/nnunet_accessroute/inference/inference_stage1/uncertainty_summary_filtered_cases.csv'
+source_folder = '/gpfs/data/fs71894/mahdi_i/ActiveLearning/dataset/nnunet_data/inference/Dataset_011/pred_proba'
+# filtered_folder = '/gpfs/data/fs71894/mahdi_i/nnunet_accessroute/inference/inference_stage1/proba'
+output_path = '/gpfs/data/fs71894/mahdi_i/ActiveLearning/dataset/nnunet_data/inference/Dataset_011/uncertainty_estimations/uncertainty_summary.csv'
 
 # File & model settings
 npz_key = 'probabilities'
@@ -23,29 +23,30 @@ very_confident_threshold = 0.9
 class_names = ['Aorta', 'Left Iliac', 'Right Iliac']
 
 # ----- PREPARE -----
-os.makedirs(filtered_folder, exist_ok=True)
+os.makedirs(source_folder, exist_ok=True)
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
 npz_files = [f for f in os.listdir(source_folder) if f.endswith('.npz')]
 # Filter out cases with no iliac
-npz_files = [f for f in npz_files if os.path.splitext(f)[0] not in cases_with_no_iliac]
+# npz_files = [f for f in npz_files if os.path.splitext(f)[0] not in cases_with_no_iliac]
 
 print(f"Found {len(npz_files)} .npz files in source folder.\n")
 
-# ----- COPY FILES TO NEW FOLDER -----
-for file in npz_files:
-    src_path = os.path.join(source_folder, file)
-    dst_path = os.path.join(filtered_folder, file)
-    shutil.copy2(src_path, dst_path)
+# # ----- COPY FILES TO NEW FOLDER -----
+# for file in npz_files:
+#     src_path = os.path.join(source_folder, file)
+#     dst_path = os.path.join(filtered_folder, file)
+#     shutil.copy2(src_path, dst_path)
 
-print(f"✅ Copied all .npz files to: {filtered_folder}\n")
+# print(f"✅ Copied all .npz files to: {filtered_folder}\n")
 
 # ----- UNCERTAINTY CALCULATION -----
 all_metrics = []
 
-for file in tqdm(npz_files, desc="Processing copied files"):
+for file in tqdm(npz_files, desc="Processing files"):
     case_id = os.path.splitext(file)[0]
 
     try:
-        data = np.load(os.path.join(filtered_folder, file))
+        data = np.load(os.path.join(source_folder, file))
         probs = data[npz_key]
         num_classes = probs.shape[0]
 
@@ -67,7 +68,7 @@ for file in tqdm(npz_files, desc="Processing copied files"):
             mean_prob = np.mean(probs_i)
             std_prob = np.std(probs_i)
             entropy = -(np.clip(probs_i, 1e-6, 1 - 1e-6) * np.log(np.clip(probs_i, 1e-6, 1 - 1e-6)) + 
-                        (1 - probs_i) * np.log(np.clip(1 - probs_i, 1e-6, 1 - 1e-6)))
+                        (1 - probs_i) * np.log(np.clip(1 - probs_i, 1e-6, 1 - 1e-6))) # formula = -p*log(p) - (1-p)*log(1-p)
             mean_entropy = np.mean(entropy)
             frac_uncertain = np.sum((probs_i > uncertain_range[0]) & (probs_i < uncertain_range[1])) / len(probs_i)
             frac_confident = np.sum(probs_i > confident_threshold) / len(probs_i)
